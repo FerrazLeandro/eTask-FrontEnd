ed82a72e866c47e76479a7de3c8944dbacb7e108
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2023-03-29 18:25:08 -0300

    Apontando para um banco no railway

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "https://etask-backend-production.up.railway.app/api"
 });
 
 export default api;
